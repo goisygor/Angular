7ff906b78415e2151058828df379635ab0a124df
goisygor <ygorgoissenai@gmail.com>
2024-02-09 19:47:36 -0300

    09/02/24

--- ProjetoAngular/src/app/app.component.ts
+++ ProjetoAngular/src/app/app.component.ts
@@ -10,4 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ProjetoAngular';
+  public tarefa = "";
+  public itens = ['Item 1','Item 2','Item 3','Item 4'];
+
+addTarefa (){
+  this.itens.push(this.tarefa);
+  
+}
+
+removeTarefa(item: string){
+  this.itens.splice(this.itens.indexOf(item),1);
+
+  
+
+}
+
 }
--- ProjetoAngular/src/app/app.routes.ts
+++ ProjetoAngular/src/app/app.routes.ts
@@ -1,22 +1,19 @@
-import { NgModule } from &#39;@angular/core&#39;;
-import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { SobreComponent } from './sobre/sobre.component';
-import { ContatoComponent } from './contato/contato.component';
+import { BrowserModule } from "@angular/platform-browser";
+import{ NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-const routes: Routes = [
-{ path: 'header', component: HeaderComponent },
-{ path: 'footer', component: FooterComponent },
-{ path: 'Home', component: HomeComponent },
-{ path: 'Sobre', component: SobreComponent },
-{ path: 'Contato', component: ContatoComponent },
-{path: "", redirectTo: '/home', pathMatch: 'full'},
-];
+import { AppComponent } from "./app.component";
 
 @NgModule({
-imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule],
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppRoutingModule {}
+
+export class AppModule { }
